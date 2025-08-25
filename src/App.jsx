d5bbcc16cbef3ae039b9d74b89b8898d0684ff34
nathanielpierson <nathanielepierson@gmail.com>
2025-08-25 16:19:20 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <p>
+        hello world
+      </p>
+    </div>
+  )
+}
+
+export default App
